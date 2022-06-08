11cd67ade738778a62d57e971deb620b8a8fb0fa
flotix8 <98894517+flotix8@users.noreply.github.com>
2022-06-08 13:32:37 +0200

    Add files via upload

---
+++ HTML JAVA.js
@@ -0,0 +1,7 @@
+function something(){
+    console.log(prompt('What is your name or nickname ?'));
+    console.log(prompt('Do you like this web site ?'));
+    console.log(parseInt(prompt('How old are you ?')));
+    console.log(prompt('What is your favorite video game ?'));
+    console.log(prompt('Do you like the new technologies ?'));
+}
